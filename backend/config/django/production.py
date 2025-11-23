ac63685a85e470303aa89c095a1f34a24126d6c0
artem <SS4T46@yandex.ru>
2025-11-23 12:45:25 +0300

    Fix compose

--- backend/config/django/production.py
+++ backend/config/django/production.py
@@ -2,4 +2,18 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS",default=[])
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS", default=[])
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databasesu
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
