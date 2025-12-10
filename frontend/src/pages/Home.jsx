381055e978ccbb0ecd1d150be85867db6c26b9ac
artem <SS4T46@yandex.ru>
2025-12-10 23:41:31 +0300

    Home was updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
 import Layout from "../components/Layout"
+import Box from '@mui/material/Box'
+import { useState, useEffect } from 'react'
+import Skeleton from '@mui/material/Skeleton'
+import api from '../api'
+
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null)
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get("/api/people");
+        setData(response.data);
+      } catch (err) {
+        console.log(err)
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [])
+
   return (
     <Layout>
-      <div>Home</div>
+      <Box sx={{ display: "flex", justifyContent: "center" }}><b>Peoples</b></Box>
+      <Box sx={{ mt: 2, display: "flex", flexGrow: 1, minHeight: 0 }}>
+        {loading ?
+          (<Skeleton variant="rounded" animation="wave" width={"100%"} height="calc(100vh - 120px)" />
+          ) : (
+            data && Array.isArray(data) && (
+              <ul>
+                {data.map((person, index) => (
+                  <li key={index}>{JSON.stringify(person)}</li>
+                ))}
+              </ul>
+            ))}
+      </Box>
     </Layout>
   )
 }
 
-export default Home
+export default Home;
